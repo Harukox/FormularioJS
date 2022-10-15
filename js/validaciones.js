08df2f29e06d5401c629346605bb5a5c64c044b0
Sara <100088307+Harukox@users.noreply.github.com>
2022-10-15 18:43:35 +0200

    validaciones de campo del formulario

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,3 +1,5 @@
+/* funciones para navegar */
+
 function irFormulario() {
     location.href="./formulario.html"
 }
@@ -6,15 +8,52 @@ function volver() {
     location.href="./index.html"
 }
 
-function validar() {
-    let personas = document.forms["formulario"]["numeroP"].value;
+
+
+//funcion para validar el numero de personas permitidas en el escape room
+function validarPersonas(personas) {
+    if(personas>1 && personas<6){
+
+        return true;
+
+    }else{
+        alert("El número de mortales tiene que ser entre 2 y 6");
+        return false;
+    }
+
+}
+//funcion para validar las fechas.
+function validarFecha(fecha){
+
+
+var ano = document.getElementById("ano").value;
+var mes = document.getElementById("mes").value;
+var dia = document.getElementById("dia").value;
+
+valor = new Date(ano, mes, dia);
+
+if( !isNaN(valor) ) {
+  return false;
+}
     
-    return (personas>1 && personas<6);
+}
+//funcion para validar el nombre y el apellido que no se puedan ingresar otro tipo de caracteres
+function validarTexto(texto){
+//expresion regular para los caracteres.
+var expresionR =  / ^ [a-zA-Z] + [a-zA-Z] + $ / ;
+
+if(expresionR.test(texto)){
+
+    return true;
+
+}else{
+    alert("introduce un texto valido");
+    return false;
+}
 }
 
 
-
-// validar dni
+//funcion para validar el dni, y que sea un dni valido.
 function validateDNI(dni) {
     var numero, let, letra;
     var expresion_regular_dni = /^[XYZ]?\d{5,8}[A-Z]$/;
@@ -42,3 +81,41 @@ function validateDNI(dni) {
         return false;
     }
 }
+
+//funcion para validar el telefono y que se introduzca un valor valido
+function validarTelefono(telefono){
+
+    if( (/^\d{9}$/.test(telefono)) ) {
+      return true;
+    }else{
+        alert("introduce un telefono valido");
+    }
+
+}
+// funcion para validar el email  para que se introduzcan valores validos.
+function validarEmail(email){
+
+    valorEmail = document.getElementById("campo").value;
+if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(valor)) ) {
+  return false;
+}
+}
+
+//funcion para validar todos los campos. 
+function validar() {
+    let personas = document.forms["formulario"]["numeroP"].value;
+    let fecha = document.forms["formulario"]["fecha"].value;
+    let nombre = document.forms ["formulario"]["nombre"].value;
+    let apellido =document.forms["formulario"]["apellidos"].value;
+    let dni =document.forms["formulario"]["dni"].value;
+    let telefono =document.forms["formulario"]["telefono"].value;
+    let email =document.forms["formulario"]["email"].value;
+    
+    
+    return validarTexto(nombre) && validarTexto(apellido) &&
+    validateDNI(dni) && validarPersonas(personas) && validarEmail(email) && 
+    validarTelefono(telefono) && validarFecha(fecha);
+
+    
+}
+
